39e8ae5f6b2dd4ef438118e36cad900fcdbeba62
Abhimanyu Malik <abhimanyumalik369@gmail.com>
2019-05-28 12:50:41 +0530

    added database query

--- admin/home.php
+++ admin/home.php
@@ -1,6 +1,4 @@
-<?php
-    echo "welcome to home";
-?>
+
 <?php session_start(); ?>
 <?php if(isset($_SESSION['username'])){ ?>
     
@@ -9,13 +7,64 @@
 <head>
     <meta charset="UTF-8">
     <title>Admin Panel</title>
+    
+    <style>
+        nav {
+            display: flex;
+            justify-content: center;
+            
+            background-color: darkgray;
+            padding: 5px;
+        }
+        
+        .btnn {
+            padding: 5px 10px;
+            background-color: white;
+            border-radius: 5px;
+            text-decoration: none;
+        }
+        
+        .btnn-logout {
+            
+        }
+        
+        .btnn-uploading {
+            text-align: center;    
+            display: flex;
+            flex-direction: column;
+        }
+        
+        .btnn-img-upload {
+            border: 1px solid black;
+            align-self: center;
+            margin-bottom: 5px;
+        }
+        
+        .echo {
+            text-align: center;
+        }
+        
+        h1 {
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
-    <h1>Admin Panel</h1>
-    <a href="upload_images.php">Upload Images</a>
-    <a href="upload_album.php">Upload Album</a>
+   <nav>
+       <a class="btnn btnn-logout" href="logout.php">Logout</a>
+   </nav>
+   <div class="echo">
+       <?php
+            echo "welcome to home";
+       ?>
+   </div>
     
-    <a href="logout.php">Logout</a>
+    <h1>Admin Panel</h1>
+    
+    <div class="btnn-uploading">
+        <a class="btnn btnn-img-upload" href="upload_images.php">Upload Images</a>
+        <a class="btnn btnn-img-upload" href="upload_album.php">Upload Album</a>
+    </div>
     
 </body>
 </html>    
--- admin/upload_images.php
+++ admin/upload_images.php
@@ -1,5 +1,7 @@
 <?php session_start(); ?>
-<?php $_SESSION['username']; ?>
+<?php if(!isset($_SESSION['username'])){
+    header("location: ./login.php");
+} ?>
     
 
 <?php
@@ -9,7 +11,7 @@
 
     require "./db.php";
     require "./Vendor/php/class.upload.php";
-    
+    //echo date("d-m-Y H:i:s");
     if(isset($_POST['submit'])){
         // Image Count
         $total = count($_FILES['image_fields']['name']);
@@ -30,7 +32,7 @@
               $handle->file_new_name_ext    = pathinfo($_FILES['image_fields']['name'][$i], PATHINFO_EXTENSION); 
                   
               // Uploading Path    
-              $handle->process('./files/raw/');
+              $handle->process('../files/raw/');
                 
               /////////////////////////////     
               // Resized Image - Large    
@@ -38,7 +40,7 @@
                 
               // File Naming      
               $handle->file_new_name_body   = pathinfo($_FILES['image_fields']['name'][$i], PATHINFO_FILENAME );
-              $handle->file_name_body_add = '_resized';
+              //$handle->file_name_body_add = '_resized';
               $handle->file_new_name_ext    = pathinfo($_FILES['image_fields']['name'][$i], PATHINFO_EXTENSION);
                 
               // Resizing of image    
@@ -47,12 +49,17 @@
               $handle->image_y              = 1920;
               $handle->image_x              = 1920;
                 
-              // Compression of image    
+              // Compression of image 
+                
+              //https://stackoverflow.com/questions/415801/allowed-memory-size-of-33554432-bytes-exhausted-tried-to-allocate-43148176-byte
+              // It will take unlimited memory usage of server     
+              ini_set('memory_limit', '-1');
+                
               $handle->image_convert = 'jpg';
               $handle->jpeg_quality = 80;  
                   
               // Uploading Path    
-              $handle->process('./files/');
+              $handle->process('../files/');
                 
                 
               /////////////////////////////     
@@ -61,7 +68,7 @@
                 
               // File Naming      
               $handle->file_new_name_body   = pathinfo($_FILES['image_fields']['name'][$i], PATHINFO_FILENAME );
-              $handle->file_name_body_add = '_resized_thumb_large';
+              $handle->file_name_body_pre = 'thumb_large_';
               $handle->file_new_name_ext    = pathinfo($_FILES['image_fields']['name'][$i], PATHINFO_EXTENSION);
                 
               // Resizing of image    
@@ -70,12 +77,13 @@
               $handle->image_y              = 800;
               $handle->image_x              = 800;
                 
-              // Compression of image    
+              // Compression of image
+              ini_set('memory_limit', '-1');    
               $handle->image_convert = 'jpg';
               $handle->jpeg_quality = 95;  
                   
               // Uploading Path    
-              $handle->process('./files/thumb_large');
+              $handle->process('../files/thumb_large');
                 
               /////////////////////////////     
               // Resized Image - Thumb Small    
@@ -83,7 +91,7 @@
                 
               // File Naming      
               $handle->file_new_name_body   = pathinfo($_FILES['image_fields']['name'][$i], PATHINFO_FILENAME );
-              $handle->file_name_body_add = '_resized_thumb_small';
+              $handle->file_name_body_pre = 'thumb_small_';
               $handle->file_new_name_ext    = pathinfo($_FILES['image_fields']['name'][$i], PATHINFO_EXTENSION);
                 
               // Resizing of image    
@@ -92,14 +100,29 @@
               $handle->image_y              = 400;
               $handle->image_x              = 400;
                 
-              // Compression of image    
+              // Compression of image 
+              ini_set('memory_limit', '-1');    
               $handle->image_convert = 'jpg';
               $handle->jpeg_quality = 100;  
                   
               // Uploading Path    
-              $handle->process('./files/thumb_small');    
-
+              $handle->process('../files/thumb_small');    
+              
+              // To increase the excecution time    
+              ini_set('max_execution_time', 300);
+                
               if ($handle->processed) {
+                $date = date("Y-m-d H:i:s");
+                $img_url_raw = "files/raw/" . $_FILES['image_fields']['name'][$i];  
+                $img_url = "files/" .$_FILES['image_fields']['name'][$i];  
+                $db->insert(
+                    'images', [
+                        'img_url_raw' => $img_url_raw,
+                        'img_url' => $img_url,
+                        'date' => $date
+                    ]
+                );  
+                  
                 echo 'Uploading Succesfull !!!';
                 $handle->clean();
               } else {
@@ -118,8 +141,28 @@
 <head>
     <meta charset="UTF-8">
     <title>Upload Images</title>
+    
+    <style>
+        nav {
+            display: flex;
+            justify-content: center;
+            
+            background-color: darkgray;
+            padding: 5px;
+        }
+        
+        .btnn {
+            padding: 5px 10px;
+            background-color: white;
+            border-radius: 5px;
+            text-decoration: none;
+        }
+    </style>
 </head>
 <body>
+    <nav>
+        <a class="btnn btnn-logout" href="logout.php">Logout</a>
+    </nav>
     <h1>Upload Images</h1>
     <form enctype="multipart/form-data" method="post" action="upload_images.php">
       <input type="file" size="32" name="image_fields[]" value="" multiple>
